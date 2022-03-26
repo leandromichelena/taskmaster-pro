b848c7a7189eeba8c13d814742f9881c4cea4fbd
Leandro Michelena <leandromichelena@gmail.com>
2022-03-26 00:24:23 -0400

    Added drag and drop feature to reorder and move tasks between lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -186,6 +186,58 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// adds drag and drop functionality
+$(".card .list-group").sortable({ // selects elements with the class .list-group within the class .card and makes it sortable with JQueryUI
+  // connect with is a property of the sortable widget to define where these can be dropped
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+  // trim down list's ID to match object property
+  var arrName = $(this)
+    .attr("id") // reads the attribute "id" from the list
+    .replace("list-", "");
+
+  // update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();}
+});
+
 // load tasks for the first time
 loadTasks();
 
